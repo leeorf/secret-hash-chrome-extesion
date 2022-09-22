83afb03b9b7abbfbc368cf00af0b3c0049a82453
Leonardo Rodrigues <leo.rf.dev@gmail.com>
2022-09-21 23:51:29 -0300

    feat: add chrome storage to manage hash input visibility and value
    Also start to share data between popup page and options page.

--- options.js
+++ options.js
@@ -2,7 +2,21 @@ const alwaysHideInputSwitchElement = document.getElementById(
   "always-hide-input-switch"
 );
 
+chrome.storage.sync.get(["shouldAlwaysHideInput"], (result) => {
+  // Safety check to avoid unexpected behavior due to undefined being a falsy value
+  const shouldAlwaysHideInput = result.shouldAlwaysHideInput ?? true;
+
+  alwaysHideInputSwitchElement.setAttribute(
+    "aria-checked",
+    shouldAlwaysHideInput
+  );
+});
+
 alwaysHideInputSwitchElement.addEventListener("click", (e) => {
-  const checked = e.target.ariaChecked === "true";
-  e.target.setAttribute("aria-checked", !checked);
+  const currentValue = e.target.ariaChecked === "true";
+  const newValue = !currentValue;
+  e.target.setAttribute("aria-checked", !currentValue);
+  chrome.storage.sync.set({
+    shouldAlwaysHideInput: newValue,
+  });
 });
--- popup.js
+++ popup.js
@@ -1,10 +1,20 @@
 const hashTargetElement = document.getElementById("hash-target");
+const saveButtonElement = document.getElementById("save-button");
 
-chrome.action.setBadgeText(
-  {
-    text: hashTargetElement.value ? "filled" : "",
-  },
-  () => {
-    console.log("finish");
-  }
-);
+chrome.storage.sync.get(["shouldAlwaysHideInput", "hashTarget"], (result) => {
+  // Safety check to avoid unexpected behavior due to undefined being a falsy value
+  const shouldAlwaysHideInput = result.shouldAlwaysHideInput ?? "password";
+  const hashTarget = result.hashTarget ?? "";
+
+  hashTargetElement.value = hashTarget;
+  hashTargetElement.setAttribute(
+    "type",
+    shouldAlwaysHideInput ? "password" : "text"
+  );
+});
+
+saveButtonElement.addEventListener("click", () => {
+  chrome.storage.sync.set({
+    hashTarget: hashTargetElement.value,
+  });
+});
