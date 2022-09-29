8053d7699f31566be6e0fca2ad3af38aac104d85
Leonardo Rodrigues <leo.rf.dev@gmail.com>
2022-09-28 22:05:11 -0300

    style: format codebase with prettier

--- options.js
+++ options.js
@@ -1,43 +1,43 @@
 const alwaysHideInputSwitchElement = document.getElementById(
-  "always-hide-input-switch"
+  'always-hide-input-switch'
 );
 const showNotificationSwitchElement = document.getElementById(
-  "show-notification-switch"
+  'show-notification-switch'
 );
 
 chrome.storage.sync.get(
-  ["shouldAlwaysHideInput", "showNotification"],
-  (result) => {
+  ['shouldAlwaysHideInput', 'showNotification'],
+  result => {
     // Safety check to avoid unexpected behavior due to undefined being a falsy value
     const shouldAlwaysHideInput = result.shouldAlwaysHideInput ?? true;
     const showNotification = result.showNotification ?? true;
 
     alwaysHideInputSwitchElement.setAttribute(
-      "aria-checked",
+      'aria-checked',
       shouldAlwaysHideInput
     );
     showNotificationSwitchElement.setAttribute(
-      "aria-checked",
+      'aria-checked',
       showNotification
     );
   }
 );
 
-const toggleValue = (target) => {
-  const currentValue = target.ariaChecked === "true";
+const toggleValue = target => {
+  const currentValue = target.ariaChecked === 'true';
   const newValue = !currentValue;
-  target.setAttribute("aria-checked", !currentValue);
+  target.setAttribute('aria-checked', !currentValue);
   return newValue;
 };
 
-alwaysHideInputSwitchElement.addEventListener("click", (e) => {
+alwaysHideInputSwitchElement.addEventListener('click', e => {
   const newValue = toggleValue(e.target);
   chrome.storage.sync.set({
     shouldAlwaysHideInput: newValue,
   });
 });
 
-showNotificationSwitchElement.addEventListener("click", (e) => {
+showNotificationSwitchElement.addEventListener('click', e => {
   const newValue = toggleValue(e.target);
   chrome.storage.sync.set({
     showNotification: newValue,
--- popup.js
+++ popup.js
@@ -1,40 +1,47 @@
-const hashTargetElement = document.getElementById("hash-target");
-const saveButtonElement = document.getElementById("save-button");
+const hashTargetElement = document.getElementById('hash-target');
+const saveButtonElement = document.getElementById('save-button');
 
-chrome.storage.sync.get(["shouldAlwaysHideInput", "hashTarget"], (result) => {
+chrome.storage.sync.get(['shouldAlwaysHideInput', 'hashTarget'], result => {
   // Safety check to avoid unexpected behavior due to undefined being a falsy value
-  const shouldAlwaysHideInput = result.shouldAlwaysHideInput ?? "password";
-  const hashTarget = result.hashTarget ?? "";
+  const shouldAlwaysHideInput = result.shouldAlwaysHideInput ?? 'password';
+  const hashTarget = result.hashTarget ?? '';
 
   hashTargetElement.value = hashTarget;
   hashTargetElement.setAttribute(
-    "type",
-    shouldAlwaysHideInput ? "password" : "text"
+    'type',
+    shouldAlwaysHideInput ? 'password' : 'text'
   );
 });
 
-saveButtonElement.addEventListener("click", () => {
+saveButtonElement.addEventListener('click', async () => {
   chrome.storage.sync.set({
     hashTarget: hashTargetElement.value,
   });
 
   try {
-    navigator.clipboard.writeText(hashTargetElement.value);
+    const hashResult = await hashMessage(hashTargetElement.value);
 
-    chrome.storage.sync.get(["showNotification"], (result) => {
-      // Safety check to avoid unexpected behavior due to undefined being a falsy value
-      const showNotification = result.showNotification ?? true;
+    navigator.clipboard.writeText(hashResult);
 
-      if (showNotification) {
-        chrome.notifications.create({
-          type: "basic",
-          title: "Good!",
-          message: "The hash have been copied to your clipboard",
-          iconUrl: "icon.png",
-        });
-      }
-    });
+    const hashResultElement = document.getElementById('hash-result');
+    hashResultElement.textContent = hashResult;
   } catch (err) {
     console.error(err);
   }
 });
+
+const hashMessage = async message => {
+  // encode as (utf-8) Uint8Array
+  const msgUint8 = new TextEncoder().encode(message);
+
+  // hash the message
+  const hashBuffer = await crypto.subtle.digest('SHA-1', msgUint8);
+
+  // convert buffer to byte array
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+  // convert bytes to hex string
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+  return hashHex;
+};
